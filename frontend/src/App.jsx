9121c3b43ba300b7f62284055ce2efbff9849730
Nkugavarathan <kugavarathan28@gmail.com>
2025-05-29 00:19:50 +0530

    setuped tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,30 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import NoteDetailPage from "./pages/NoteDetailPage"
+import { Toaster, toast } from "react-hot-toast"
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <div>
+      {/* Add Toaster to render the toast notifications */}
+      <Toaster />
+
+      {/* A sample button to trigger a toast */}
+
+      <button
+        onClick={() => toast.success("Successfully!")}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Clickme
+      </button>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  )
 }
