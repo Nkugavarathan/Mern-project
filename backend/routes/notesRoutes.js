e983fa3c11d15cdcd0dc615a20fb633282d02767
Nkugavarathan <kugavarathan28@gmail.com>
2025-05-28 11:04:32 +0530

    rotes and controlled created

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,1 +1,20 @@
-cd
+import express from "express"
+
+const router = express.Router()
+
+router.get("/", (req, res) => {
+  res.status(200).send("u fetched the note")
+})
+
+router.post("/", (req, res) => {
+  res.status(201).json({ message: "u fetched the note" })
+})
+
+router.put("/:id", (req, res) => {
+  res.status(201).json({ message: "Note updated succesffully" })
+})
+router.delete("/:id", (req, res) => {
+  res.status(201).json({ message: "Note deleted succesffully" })
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,30 @@
-const express = require("express")
+import express from "express"
+import noteRoutes from "./routes/notesRoutes.js" // note the .js extension
+
 const app = express()
 
-app.use(express.json())
+app.use(express.json()) // to parse JSON bodies
 
-const cors = require("cors")
-app.use(cors())
+// Use CORS if needed
+// import cors from "cors";
+// app.use(cors());
 
+app.use("/api/notes", noteRoutes)
 app.get("/", (req, res) => {
-  res.status(200).send("Hello Wors")
+  res.send("Welcome to the MERN Notes App!")
 })
-app.post("/posts", (req, res) => {
-  console.log(req.body)
-  res.json({ message: "User created" })
-})
+// app.use(express.json())
+
+// const cors = require("cors")
+// app.use(cors())
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("Hello Wors")
+// })
+// app.post("/posts", (req, res) => {
+//   console.log(req.body)
+//   res.json({ message: "User created" })
+// })
 
 app.listen(3000, () => {
   console.log("App running on http://localhost:3000")
