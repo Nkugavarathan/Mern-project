8a361faa17976352822604b4fa8328352cc0b859
Nkugavarathan <kugavarathan28@gmail.com>
2025-05-29 09:22:17 +0530

    daisyui package installed with tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,23 +3,29 @@ import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import CreatePage from "./pages/CreatePage"
 import NoteDetailPage from "./pages/NoteDetailPage"
-import { Toaster, toast } from "react-hot-toast"
+// import { Toaster, toast } from "react-hot-toast"
 
+import Navbar from "./components/Navbar"
 export default function App() {
   return (
+    //     Add Toaster to render the toast notifications
+    //     <Toaster />
+    //     {/* A sample button to trigger a toast */}
+    //     /* <button
+    //       onClick={() => toast.success("Successfully!")}
+    //       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    //     >
+    //       Clickme
+    //     </button> */
+    //     /* <div className="p-4">
+    //       <button className="btn btn-primary">DaisyUI Button</button>
+    //     </div> */
+    //     /* <h1 class="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1> */
+
     <div>
-      {/* Add Toaster to render the toast notifications */}
-      <Toaster />
-
-      {/* A sample button to trigger a toast */}
-
-      <button
-        onClick={() => toast.success("Successfully!")}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Clickme
-      </button>
-
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
