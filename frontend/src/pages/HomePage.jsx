d6a4f63a347d6c791e9aa2f1537fbc8a067f4ced
Nkugavarathan <kugavarathan28@gmail.com>
2025-05-30 10:44:59 +0530

    NoteCard componendt created

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar"
 import RateLimitedUI from "./../components/RateLimitedUI"
 import toast from "react-hot-toast"
 import axios from "axios"
+import NoteCard from "../components/NoteCard"
 export default function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(false)
   const [notes, setNotes] = useState([])
@@ -48,15 +49,19 @@ export default function HomePage() {
   return (
     <div className="min-h-screen">
       <Navbar />
-      <h1>My Notes</h1>
-      {notes.map((note) => (
-        <div key={note._id}>
-          <h1>
-            <strong> {note.title}</strong>
-          </h1>
-          <p>{note.description}</p>
-        </div>
-      ))}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading Notes...</div>
+        )}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        )}
+      </div>
+
       {isRateLimited && <RateLimitedUI />}
     </div>
   )
