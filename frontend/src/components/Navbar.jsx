0f56ff1822eff22689f3844d872f59a284ba87b1
Nkugavarathan <kugavarathan28@gmail.com>
2025-05-31 13:19:05 +0530

    navbar header styled

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { PlusIcon } from "lucide-react"
 export default function Navbar() {
   return (
     <div>
-      <header className="bg-base-300 border-b border-base-content/10">
+      <header className="bg-white/80 backdrop-blur-md border-b border-gray-200 shadow-sm">
         <div className="mx-auto max-w-6xl p-4">
           <div className="flex items-center justify-between">
             <h1 className="text-primary">Note App</h1>
